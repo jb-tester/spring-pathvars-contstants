d8b92ad66866b4ebf3ffd78904b3e0d92b3de59b
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2020-02-26 16:58:06 +0300

    few templates in one path fragment

--- src/main/java/com/mytests/spring/constAsPaths/MyRestController.java
+++ src/main/java/com/mytests/spring/constAsPaths/MyRestController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +22,8 @@ public class MyRestController {
         log.info(foo+b+dummy);
     }
 
+    @RequestMapping("/aaa/{pv1}_{pv2}{pv3}/ccc")
+    public String aaa(@PathVariable String pv1, @PathVariable String pv2, @PathVariable String pv3) {
+        return "aaa";
+    }
 }
