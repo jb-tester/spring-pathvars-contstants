1b06424a2d3fc4b9f1cd6b872705081c68ed32f9
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2020-02-26 14:59:04 +0300

    IDEA_121150_Controller: remove comments

--- src/main/java/com/mytests/spring/constAsPaths/IDEA_121150_Controller.java
+++ src/main/java/com/mytests/spring/constAsPaths/IDEA_121150_Controller.java
@@ -5,36 +5,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IDEA_121150_Controller {
-    public static final String CONST = "/const";
+public class  IDEA_121150_Controller {
+    public static final String MYCONST = "/const";
     public static final String EXPLICIT = "/testExplicitConst/{arg}";
 
-    @RequestMapping("/testExplicit/{arg}") // works
+    @RequestMapping("/testExplicit/{arg}")
     public String testExplicit(@PathVariable("arg") final String arg) {
         return arg;
     }
 
-    @RequestMapping("/testImplied/{arg}") // works
+    @RequestMapping("/testImplied/{arg}")
     public String testImplied(@PathVariable final String arg) {
         return arg;
     }
 
-    @RequestMapping("/shouldGiveWaring") // no warning (and should give one)
+    @RequestMapping("/shouldGiveWaring")
     public String shouldGiveWaring(@PathVariable final String arg) {
         return arg;
     }
 
-    @RequestMapping(EXPLICIT) // Cannot resolve path variable 'arg' in @RequestMapping
+    @RequestMapping(EXPLICIT)
     public String testExplicitConst(@PathVariable("arg") final String arg) {
         return arg;
     }
 
-    @RequestMapping(CONST + "/{arg}") // Cannot resolve path variable 'arg' in @RequestMapping
+    @RequestMapping(MYCONST + "/{arg}")
     public String testFrontConst(@PathVariable("arg") final String arg) {
         return arg;
     }
 
-    @RequestMapping("/{arg}" + CONST) // Cannot resolve path variable 'arg' in @RequestMapping
+    @RequestMapping("/{arg}" + MYCONST)
     public String testTailConst(@PathVariable("arg") final String arg) {
         return arg;
     }
